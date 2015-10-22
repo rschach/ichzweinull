e74dbb7df5b2940e9559b7dd526ad4314f688605
rschach <info@ichzweinull.com>
2015-10-22 22:45:21 +0200

    Header layout

---
+++ js/functions.js
@@ -0,0 +1,30 @@
+(function($){
+  var $body = $('html, body'),
+      $window = $(window),
+      $menuLink = $('nav ul li a');
+
+  function firstLinkActive () {
+    $menuLink.first().addClass('active');
+  }
+
+  function onMenuLinkClick (e) {
+    var $this = $(this),
+        href = $this.attr('href'),
+        $target = $(href);
+
+    if ($target.length == 0)
+        return;
+
+    e.preventDefault();
+
+    $body.animate({
+        scrollTop: $target.offset().top-70
+    }, 300);
+  }
+
+  $(document).ready(function(){
+    firstLinkActive();
+    $('a[href^="#"]').on('click', onMenuLinkClick);
+  });
+
+})(jQuery)
